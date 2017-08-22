0b43570ee4cbeb4d634d89b1f7dc6493bfccb06b
Joyqul Chen <joyqul@skyyer.com>
2017-08-23 04:01:55 +0800

    Can divide multiple image now

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 saveDirectory = 'Desktop/'
 fileName = 'Desktop/tmpfile.jpg'
 savedFileFormat = 'jpeg'
-from PIL import Image
 
+class Image:
+    def __init__(self, filename, columns=1, rows=1, ext='jpg'):
+        self.filename = filename
+        self.columns = columns
+        self.rows = rows
+        self.ext = ext
+        self.save_basename()
 
-def get_size_after_slice(originSize, pieces):
-	newSize = originSize//pieces
-	redundant = originSize-newSize*pieces
-	return newSize, redundant
+    def save_basename(self):
+        import os
+        self.basename = os.path.splitext(os.path.basename(filename))[0]
 
-def slice(columns=1, rows=1):
-	imageData = Image.open(fileName)
-	imageWidth, imageHeight = imageData.size
+    def get_size_after_slice(self, originSize, pieces):
+        newSize = originSize//pieces
+        redundant = originSize-newSize*pieces
+        return newSize, redundant
 
-	newWidth, widthRedundant = get_size_after_slice(imageWidth, columns)
-	newHeight, heightRedundant = get_size_after_slice(imageHeight, rows)
-	#TODO: figure out how to deal if has redundant
+    def slice(self):
+        from PIL import Image
+        imageData = Image.open(self.filename)
+        imageWidth, imageHeight = imageData.size
+    
+        newWidth, widthRedundant = self.get_size_after_slice(imageWidth, self.columns)
+        newHeight, heightRedundant = self.get_size_after_slice(imageHeight, self.rows)
 
-	for i in xrange(0, columns):
-		for j in xrange(0, rows):
-			startX = newWidth*i
-			startY = newHeight*j
-			endX = startX+newWidth-1
-			endY = startY+newHeight-1
-			area = (startX, startY, endX, endY)
-			cropedImage = imageData.crop(area)
-			newFilename = 'slice_%s_%s.jpg' %(i, j)
-			cropedImage.save(newFilename, savedFileFormat)
+        #TODO: figure out how to deal if has redundant
+        if widthRedundant:
+            print 'Has redundant %s in width' %( widthRedundant )
+        if heightRedundant:
+            print 'Has redundant %s in height' %( heightRedundant )
+    
+        for i in xrange(0, self.columns):
+            for j in xrange(0, self.rows):
+                startX = newWidth*i
+                startY = newHeight*j
+                endX = startX+newWidth-1
+                endY = startY+newHeight-1
+                area = (startX, startY, endX, endY)
+                cropedImage = imageData.crop(area)
+                newFilename = '%s_%s_%s.%s' %(self.basename, i, j, self.ext)
+                cropedImage.save(newFilename, savedFileFormat)
 
-slice(rows=2)
+testImages = ['tmpfile.jpg', 'tmpfile2.jpg']
+for filename in testImages:
+    tmp = Image(filename, rows=2)
+    tmp.slice()
